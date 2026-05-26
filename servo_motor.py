56b779c1347e8fc8bb5152adbb8e55fab37051b5
HoangAnhPham-swnbrn <104686543@student.swin.edu.au>
2026-05-27 09:35:07 +1000

    Update servo_motor.py

--- servo_motor.py
+++ servo_motor.py
@@ -1,42 +1,13 @@
 import RPi.GPIO as GPIO
 import time
 
-SERVO_PIN = 19
-
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(SERVO_PIN, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
 
-pwm = GPIO.PWM(SERVO_PIN, 50)
-pwm.start(0)
+pwm = GPIO.PWM(19, 50)
+pwm.start(7.5)
 
-def move(angle):
-    duty = 2 + (angle / 18)
-    pwm.ChangeDutyCycle(duty)
-    time.sleep(0.5)
-    pwm.ChangeDutyCycle(0)
+time.sleep(2)
 
-try:
-    print("Testing Servo...")
-
-    print("Center")
-    move(90)
-    time.sleep(1)
-
-    print("Left")
-    move(0)
-    time.sleep(1)
-
-    print("Right")
-    move(180)
-    time.sleep(1)
-
-    print("Center")
-    move(90)
-
-except KeyboardInterrupt:
-    pass
-
-finally:
-    pwm.stop()
-    GPIO.cleanup()
-    print("Done.")
+pwm.stop()
+GPIO.cleanup()
