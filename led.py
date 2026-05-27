7094006b8c016a36d0919c0d8ccc4d303228daa5
HoangAnhPham-swnbrn <104686543@student.swin.edu.au>
2026-05-27 17:32:21 +1000

    Update led.py

--- led.py
+++ led.py
@@ -1,27 +1,40 @@
 import lgpio
 import time
 
-BLINKER = 26
+LEFT_BLINKER  = 20
+RIGHT_BLINKER = 21
 BLINK_INTERVAL = 0.5
 
 chip = lgpio.gpiochip_open(0)
-lgpio.gpio_claim_output(chip, BLINKER)
+lgpio.gpio_claim_output(chip, LEFT_BLINKER)
+lgpio.gpio_claim_output(chip, RIGHT_BLINKER)
 
 try:
     print("=== Blinker Test ===")
 
-    for _ in range(10):
-        lgpio.gpio_write(chip, BLINKER, 1)
+    print("\nLeft Blinker")
+    for _ in range(6):
+        lgpio.gpio_write(chip, LEFT_BLINKER, 1)
         time.sleep(BLINK_INTERVAL)
-        lgpio.gpio_write(chip, BLINKER, 0)
+        lgpio.gpio_write(chip, LEFT_BLINKER, 0)
         time.sleep(BLINK_INTERVAL)
 
-    print("Done.")
+    time.sleep(1)
+
+    print("\nRight Blinker")
+    for _ in range(6):
+        lgpio.gpio_write(chip, RIGHT_BLINKER, 1)
+        time.sleep(BLINK_INTERVAL)
+        lgpio.gpio_write(chip, RIGHT_BLINKER, 0)
+        time.sleep(BLINK_INTERVAL)
+
+    print("\nDone.")
 
 except KeyboardInterrupt:
     print("\nStopped by user.")
 
 finally:
-    lgpio.gpio_write(chip, BLINKER, 0)
+    lgpio.gpio_write(chip, LEFT_BLINKER,  0)
+    lgpio.gpio_write(chip, RIGHT_BLINKER, 0)
     lgpio.gpiochip_close(chip)
     print("Cleanup done.")
